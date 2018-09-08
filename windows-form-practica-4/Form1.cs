2d863b23ad3b7f31eb64ce2cc08bcc51e292fabd
Gaston Pesoa <gastonpesoa@gmail.com>
2018-09-08 18:35:28 -0300

    Se agrega practica 4 CheckBox MessageBox

---
+++ windows-form-practica-4/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windows_form_practica_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int total = 0;
+
+            if (chkMonitor.Checked == true)
+            {
+                total += 250;
+            }
+
+            if (chkTeclado.Checked == true)
+            {
+                total += 15;
+            }
+
+            if (chkMouse.Checked == true)
+            {
+                total += 20;
+            }
+
+            MessageBox.Show("El total es: " + total.ToString());
+        }
+
+        private void chkMonitor_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkMonitor.Checked == true)
+            {
+                lblMensaje.Text = "Monitor";
+            }
+            else
+            {
+                lblMensaje.Text = "";
+            }
+        }
+    }
+}

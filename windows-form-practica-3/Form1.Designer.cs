d727f523a25164e5bf48d4b462ee6537006d5ecb
Gaston Pesoa <gastonpesoa@gmail.com>
2018-09-08 18:10:59 -0300

    Se crea proyecto 3 calculadora basica

---
+++ windows-form-practica-3/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace windows_form_practica_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(12, 20);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(17, 13);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "A:";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(37, 17);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 1;
+            this.txtA.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtA.TextChanged += new System.EventHandler(this.txtA_TextChanged);
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(13, 64);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(17, 13);
+            this.lblB.TabIndex = 2;
+            this.lblB.Text = "B:";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(37, 64);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 3;
+            this.txtB.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Location = new System.Drawing.Point(162, 15);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(36, 23);
+            this.btnSuma.TabIndex = 5;
+            this.btnSuma.Text = "+";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.Location = new System.Drawing.Point(204, 15);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(36, 23);
+            this.btnResta.TabIndex = 6;
+            this.btnResta.Text = "-";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(204, 62);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(36, 23);
+            this.btnDiv.TabIndex = 8;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Location = new System.Drawing.Point(162, 62);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(36, 23);
+            this.btnMulti.TabIndex = 7;
+            this.btnMulti.Text = "*";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(102, 112);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(35, 13);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "label1";
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 143);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ windows-form-practica-3/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windows_form_practica_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblResultado.Text = "";
+            txtA.Text = "0";
+            txtB.Text = "0";
+        }
+
+        private void txtA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+            double b = Convert.ToDouble(txtB.Text);
+
+            double r = a + b;
+
+            lblResultado.Text = r.ToString();
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+            double b = Convert.ToDouble(txtB.Text);
+
+            double r = a - b;
+
+            lblResultado.Text = r.ToString();
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+            double b = Convert.ToDouble(txtB.Text);
+
+            double r = a * b;
+
+            lblResultado.Text = r.ToString();
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+            double b = Convert.ToDouble(txtB.Text);
+
+            double r = a / b;
+
+            lblResultado.Text = r.ToString();
+        }
+    }
+}

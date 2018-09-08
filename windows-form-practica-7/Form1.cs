33a7babc6029a5189012b89b430d02712a25c0f7
Gaston Pesoa <gastonpesoa@gmail.com>
2018-09-08 19:18:39 -0300

    Se agrega practica 7 Multiples formas

---
+++ windows-form-practica-7/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windows_form_practica_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            Form2 miForma = new Form2();
+
+            //miForma.Show();
+            miForma.ShowDialog();
+
+        }
+    }
+}
